8f264e60bb2b9b2e00ce4a2ff64468ec0c695185
matelazar07 <matelazar07@gmail.com>
2026-04-06 22:49:40 +0200

    RootedSyncDisp tesztelésre

--- agent.py
+++ agent.py
@@ -1,9 +1,18 @@
 class Agent:
     def __init__(self, id, node_position):
         self.id = id
-        self.node_position = node_position # Alpha in the paper
-        self.pin = 0 # Port number the agent used to enter the node, 0 if it hasn't moved yet
-        self.pout = 0 # Port number the agent will use to exit the node, 0 if it hasn't moved yet
+        self.node_position = node_position
+        self.pin = 0
+        self.pout = 0
+
+        self.settled = False
+        self.is_leader = False
+        self.is_seeker = False
+        self.parent_port = None
+        self.tree_label = None
+
+        # ÚJ: A Leader memóriája a visszalépéshez (verem)
+        self.path_stack = []
 
     def move_to_node(self, new_node_position, new_pin, new_pout):
         self.node_position = new_node_position
